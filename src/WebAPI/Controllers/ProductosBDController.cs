0a9b18087a2d04275af56ef20c3d47725eb37da2
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-05-25 18:48:14 -0500

    feat: api productos metodo get

---
+++ src/WebAPI/Controllers/ProductosBDController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RubiaDivinaWebAPI.Data;
+using RubiaDivinaWebAPI.Models;
+
+namespace RubiaDivinaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosBDController : ControllerBase
+    {
+        private readonly RubiaDbContext _context;
+
+        public ProductosBDController(RubiaDbContext context)
+        {
+            _context = context;
+        }
+
+        // Obtener todos los productos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            return await _context.Productos
+                .Include(p => p.Categoria) // Incluye la categoría si quieres los datos relacionados
+                .ToListAsync();
+        }
+    }
+}
