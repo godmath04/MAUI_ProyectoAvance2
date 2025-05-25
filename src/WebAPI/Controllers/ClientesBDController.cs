92106b280443f3734d08f602ee9e12115ba9473b
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-05-25 18:29:09 -0500

    feat: migracion db & api clientes CRUD

---
+++ src/WebAPI/Controllers/ClientesBDController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RubiaDivinaWebAPI.Data;
+using RubiaDivinaWebAPI.Models;
+
+namespace RubiaDivinaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesBDController : ControllerBase
+    {
+        private readonly RubiaDbContext _context;
+
+        public ClientesBDController(RubiaDbContext context)
+        {
+            _context = context;
+        }
+
+        //Obtener todos los clientes 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+        {
+            return await _context.Cliente.ToListAsync();
+        }
+
+        //Obtener cliente por id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cliente>> GetCliente(int id)
+        {
+            var cliente = await _context.Cliente.FindAsync(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+
+        }
+
+        //Actualizar cliente por id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCliente(int id, Cliente cliente)
+        {
+            if(id != cliente.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+
+        }
+
+        //Metodo auxiliar para verificar existencia
+        private bool ClienteExists(int id)
+        {
+            return _context.Cliente.Any(e => e.Id == id);
+        }
+
+
+        //Crear cliente
+        [HttpPost]
+        public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            cliente.FechaCreacion = DateTime.UtcNow;
+            _context.Cliente.Add(cliente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCliente", new { id = cliente.Id }, cliente);
+        }
+
+        //Borrar cliente por id
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCliente(int id)
+        {
+            var cliente = await _context.Cliente.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cliente.Remove(cliente);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ src/WebAPI/Migrations/20250525232329_MigracionValidacion01.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RubiaDivinaWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionValidacion01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Facturas_Pedidos_PedidoId",
+                table: "Facturas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Cliente_ClienteId",
+                table: "Pedidos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Productos_ProductoId",
+                table: "Pedidos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Facturas_Pedidos_PedidoId",
+                table: "Facturas",
+                column: "PedidoId",
+                principalTable: "Pedidos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Cliente_ClienteId",
+                table: "Pedidos",
+                column: "ClienteId",
+                principalTable: "Cliente",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Productos_ProductoId",
+                table: "Pedidos",
+                column: "ProductoId",
+                principalTable: "Productos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Facturas_Pedidos_PedidoId",
+                table: "Facturas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Cliente_ClienteId",
+                table: "Pedidos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pedidos_Productos_ProductoId",
+                table: "Pedidos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Facturas_Pedidos_PedidoId",
+                table: "Facturas",
+                column: "PedidoId",
+                principalTable: "Pedidos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Cliente_ClienteId",
+                table: "Pedidos",
+                column: "ClienteId",
+                principalTable: "Cliente",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pedidos_Productos_ProductoId",
+                table: "Pedidos",
+                column: "ProductoId",
+                principalTable: "Productos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
